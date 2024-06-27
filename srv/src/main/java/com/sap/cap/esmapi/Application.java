fa3ab9c6f6eb7cc9beb99c7c3b5016dbf53cfad4
Sunny Bhardwaj <sunny13nitk@gmail.com>
2024-06-27 11:43:59 +0530

    @EnableAsync on App main

--- srv/src/main/java/com/sap/cap/esmapi/Application.java
+++ srv/src/main/java/com/sap/cap/esmapi/Application.java
@@ -3,9 +3,11 @@ package com.sap.cap.esmapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableAsync
 public class Application  //Main Class
 {
 
